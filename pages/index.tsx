768c0264945fd4b619b22f2df94c304a8431074b
Emmanuel Agyemang <emmanuelagyemang3@gmail.com>
2022-07-30 17:28:29 +0100

    Commit Two

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,12 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
   return (
       <>
-      HOME PAGE
+      <div className="container">
+      <h1>Heading 1</h1>
+      <h2>Heading 2</h2>
+      <h3>Heading 3</h3>
+      <p>This a paragraph</p>
+      </div>
       </>
   )
 }
