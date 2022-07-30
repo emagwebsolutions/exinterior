7605360ad435a6660c15d3dbe16cb63e54548953
Emmanuel Agyemang <emmanuelagyemang3@gmail.com>
2022-07-30 18:58:20 +0100

    Commit Three

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
   return (
       <>
-      <div className="container">
+      <div className="container bg-danger p-3">
       <h1>Heading 1</h1>
       <h2>Heading 2</h2>
       <h3>Heading 3</h3>
