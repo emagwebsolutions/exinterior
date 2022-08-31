c26f2307959e2adac63736667cf39dd3217d43e2
Emmanuel Agyemang <emmanuelagyemang3@gmail.com>
2022-08-31 19:30:21 +0100

    Twenty Six Commit

--- components/home/Homesectionone.tsx
+++ components/home/Homesectionone.tsx
@@ -15,28 +15,27 @@ const Homesectionone = () => {
 
   const elem = Object.values(getallPost)
     .filter((v: any) => {
-      return v.cat_title+'' == 'home section one';
+      return v.cat_title + '' == 'home section one';
     })
     .map((vl: any, k: any) => {
-
-      return <div key={k} className="col-2 caption-bx">
-        <div>
-          <Image
-            src={vl.mainImage}
-            alt=""
-            layout="responsive"
-            width="2"
-            height="2"
-          />
+      return (
+        <div key={k} className="col-2 caption-bx">
+          <div>
+            <Image
+              src={vl.mainImage}
+              alt=""
+              layout="responsive"
+              width="2"
+              height="2"
+            />
+          </div>
+          <h5>{vl.title}</h5>
+          <div className="para">
+            <PortableText value={vl.body} />
+          </div>
         </div>
-        <h5>{vl.title}</h5>
-        <div className="para">
-          <PortableText value={vl.body} />
-        </div>
-      </div>
-});
-
-
+      );
+    });
 
   return (
     <section>
